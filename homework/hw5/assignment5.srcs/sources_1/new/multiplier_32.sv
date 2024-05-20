7372588c0e3a1e19ed15c003a643d9c4ee904819
whogashaga <chen081501@hotmail.com>
2024-05-20 10:47:57 -0700

    create assignment5

---
+++ homework/hw5/assignment5.srcs/sources_1/new/multiplier_32.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/20/2024 10:28:00 AM
+// Design Name: 
+// Module Name: multiplier_32
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module multiplier_32(
+
+    );
+endmodule
